7004f67c995e558d70b2dc468e85debbd67bca2b
Sean Mooney <smooney@sourceallies.com>
2014-07-23 06:17:19 -0500

    Awesome 3.5 config files.

---
+++ awesome/dev_menu.lua
@@ -0,0 +1,9 @@
+
+-- {{{ Development Tools
+local devmenu = {
+    {"android-studio", "/home/smooney/bin/android-studio"},
+    {"android"       , "/home/smooney/bin/android"},
+    {"idea-13"       , "/home/smooney/bin/intellij-13" }
+}
+
+return devmenu
---
+++ awesome/menues.lua
@@ -0,0 +1,8 @@
+
+-- {{{ Development Tools
+devmenu = {
+    {"android-studio", "/home/smooney/bin/android-studio"},
+    {"android"       , "/home/smooney/bin/android"},
+    {"idea-12"       , "/home/smooney/bin/intellij" },
+    {"idea-13"       , "/home/smooney/bin/intellij-13" }
+}
