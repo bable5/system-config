0b63748289a2fe87e27347b460509f394ac88e68
Sean Mooney <bable4@gmail.com>
2018-01-01 09:28:47 -0600

    Add yarn install script.

---
+++ puppet/yarn-install.sh
@@ -0,0 +1,7 @@
+#/usr/bin/env sh
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
+apt-get update && apt-get -y install yarn
+
